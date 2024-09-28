a8ea9b0786993c8dee075453f5548e16bde8ba44
patelmahetab9020@gmail.com <patelmahetab9020@gmail.com>
2024-09-28 15:07:54 +0530

    Return Minimum Moves

---
+++ src/TwentySeventh.java
@@ -0,0 +1,33 @@
+public class TwentySeventh {
+    //We have to find an minimum Move in
+    public static int minMoves(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        // Find the minimum and maximum values in the array
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        int minMoves = Integer.MAX_VALUE;
+
+        // Try each target between min and max
+        for (int target = min; target <= max; target++) {
+            int moves = 0;
+            for (int num : nums) {
+                moves += Math.abs(num - target);
+            }
+            minMoves = Math.min(minMoves, moves);
+        }
+
+        return minMoves;
+    }
+    public static void main(String[] args) {
+        int[] nums={1,2,3};
+        System.out.println(minMoves(nums));
+    }
+
+
+}
+//Output: 2
